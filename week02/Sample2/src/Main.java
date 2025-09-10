f75bf925c65b0cbae7bd01c9cb7eb002c2ad8d2b
2501076 <yeonjun5@kbu.ac.kr>
2025-09-10 10:45:54 +0900

    2주차

---
+++ week02/Sample2/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        String name=
+
+        name=keyboard.nextLine();
+
+
+        System.out.println("입력 받은 이름 : " + name);
+
+
+
+
+
+    }
+}
