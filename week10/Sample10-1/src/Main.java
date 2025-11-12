9db8a437b89281c9141d149699ec800fa3165551
2501076 <yeonjun5@kbu.ac.kr>
2025-11-12 16:23:00 +0900

    10주차

---
+++ week10/Sample10-1/src/Main.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner keyboard = new Scanner(System.in);
+        String[] name = {"홍길동", "이재명", "운석열", "경복대", "오세훈"};
+        int[] java = new int[name.length];
+        int total = 0;
+        float avg = 0.0f;
+
+        int i = 0;
+        while(true) {
+            System.out.printf("%s님의 JAVA성적 입력  : ",name[i]);
+            java[i] = keyboard.nextInt();
+
+            if(java[i] >= 0 && java[i] <= 100) {
+                i++;
+                if(i >= name.length) {
+                    break;
+                }
+            } else {
+                System.err.println("ERROR : 0~100 사이");
+                System.in.read();
+            }
+        }
+
+        for(i = 0; i < java.length; i++) {
+            total += java[i];
+        }
+
+        avg = (float) total / java.length;
+
+        for(i=0; i<java.length; i++) {
+            System.out.printf("%s 학생 = %3d점\n",name[i],java[i]);
+        }
+        System.out.printf("총 점 : %3d점\n",total);
+        System.out.printf("평균 : %.2f점\n",avg);
+    }
+}
