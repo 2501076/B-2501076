d23f7ea44ad40bf0e4ea5e703e0a83aa0297fd79
2501076 <yeonjun5@kbu.ac.kr>
2025-12-06 01:41:15 +0900

    13주차

---
+++ week13/Sample13-1/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] name = {"홍길동","이순신","강민성","김지민","이예린"};    //5개
+
+   // int[] score = new inr[name.length];    //5개
+   int[][] score = Input.inputData(name);
+   Compute.process(score);
+   Output.display(name,score);
+    }
+}
